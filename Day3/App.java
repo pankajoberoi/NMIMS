bd9c5cd2ccfca224043a184ecf8e755079fda49e
pankajoberoi <poberoi.ca19@chitkara.edu.in>
2026-05-21 11:10:59 +0530

    Updated

---
+++ Day3/App.java
@@ -0,0 +1,14 @@
+package Day3;
+
+public class App {
+    public static void main(String[] args) {
+        PaymentSevice payment = new PaymentSevice();
+
+        // payment.pay(500, "Pankaj@oksbi");
+
+        // payment.pay(2000, 030245612);
+
+        payment.pay(700);
+
+    }
+}
---
+++ Day3/PaymentSevice.java
@@ -0,0 +1,16 @@
+package Day3;
+
+public class PaymentSevice {
+
+    void pay(int amt) {
+        System.out.println("cash payment success");
+    }
+
+    void pay(int amt, String UpiId) {
+        System.out.println("UPI Payment Success");
+    }
+
+    void pay(int amt, double cardNumber) {
+        System.out.println("CARD payment success");
+    }
+}
